17f01f6bbfbffa8c53137bc4f0c3fc3b98b20a4a
Sergej Jakovljev <sergej.jakovljev@gmail.com>
2017-10-02 00:07:51 +0200

    Updated PropTypes to support RN16

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component  } from 'react';
+import PropTypes from 'prop-types';
 import { requireNativeComponent, NativeModules, View, TouchableHighlight } from 'react-native';
 
 const scannerModule = NativeModules['BarcodeScannerModule'];
